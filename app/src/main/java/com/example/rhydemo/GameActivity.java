b39fda9d133672e85026b77ce86ed91beb68557d
shan709 <shanshan@encompass8.cn>
2016-04-08 22:45:12 +0800

    format

--- app/src/main/java/com/example/rhydemo/GameActivity.java
+++ app/src/main/java/com/example/rhydemo/GameActivity.java
@@ -7,45 +7,49 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.KeyEvent;
 
-public class GameActivity extends MainActivity{
-	GameView gv;
+public class GameActivity extends MainActivity {
+    GameView gv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    	music.addMusic(R.raw.wanner, true);
-    	super.onCreate(savedInstanceState);
-    	gv=new GameView(this);
-    	setContentView(gv);
+        music.addMusic(R.raw.wanner, true);
+        super.onCreate(savedInstanceState);
+        gv = new GameView(this);
+        setContentView(gv);
     }
-    public void showDialog(){
-		AlertDialog.Builder builder=new AlertDialog.Builder(this);
-		builder.setIcon(R.drawable.icon1);
-		builder.setTitle("警告");
-		builder.setMessage("是否退出游戏?");
-		builder.setPositiveButton("否", null);
-		//第二个null继续游戏不做任何操作
-		builder.setNegativeButton("是", new DialogInterface.OnClickListener() {
-			
-			@Override
-			public void onClick(DialogInterface arg0, int arg1) {
-				finish();
-				
-			}
-		});
-		builder.show();
-	}
+
+    public void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.drawable.icon1);
+        builder.setTitle("警告");
+        builder.setMessage("是否退出游戏?");
+        builder.setPositiveButton("否", null);
+        //第二个null继续游戏不做任何操作
+        builder.setNegativeButton("是", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                finish();
+
+            }
+        });
+        builder.show();
+    }
+
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode==KeyEvent.KEYCODE_BACK){
-			showDialog();
-		}
-		return super.onKeyDown(keyCode, event);
-	}
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            showDialog();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
     @Override
     protected void onDestroy() {
-    	//游戏结束后将资源清空gameview 释放掉
-    	if(gv !=null){
-    		gv.isRun=false;
-    	}
-    	gv=null;
-    	super.onDestroy();
+        //游戏结束后将资源清空gameview 释放掉
+        if (gv != null) {
+            gv.isRun = false;
+        }
+        gv = null;
+        super.onDestroy();
     }
 }
