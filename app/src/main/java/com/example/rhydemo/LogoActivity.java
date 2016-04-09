1d97230b1e29afecae93c0031fabe89602c0e78f
shan709 <shanshan@encompass8.cn>
2016-04-09 16:30:33 +0800

    delete some packge

--- app/src/main/java/com/example/rhydemo/LogoActivity.java
+++ app/src/main/java/com/example/rhydemo/LogoActivity.java
@@ -1,7 +1,5 @@
 package com.example.rhydemo;
 
-import android.R.menu;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.AlphaAnimation;
